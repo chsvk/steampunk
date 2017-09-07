bbcdd457d65ac7ee0e1cd5ca19790d087d2def9d
Vamshi Krishna <chsvk21@gmail.com>
2017-09-07 18:08:22 +0530

    Working Contacts, Need to implement them in ListView

---
+++ kinetic/app/src/main/java/tech/steampunk/kinetic/data/Contact.java
@@ -0,0 +1,32 @@
+package tech.steampunk.kinetic.data;
+
+/**
+ * Created by Vamshi on 9/7/2017.
+ */
+
+public class Contact {
+
+    private String name;
+    private String number;
+
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
