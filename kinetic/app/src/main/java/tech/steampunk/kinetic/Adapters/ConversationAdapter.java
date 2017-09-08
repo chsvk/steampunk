16b49d3d8363b754a6212aeed030b76bc867f4c5
Vamshi Krishna <chsvk21@gmail.com>
2017-09-09 02:59:25 +0530

    Some more UI updates - AutoFocus Keyboard on opening Chat - changes in Chat_activity Layout
    Need To add -
    RecyclerView Adapter for Conversation_list

---
+++ kinetic/app/src/main/java/tech/steampunk/kinetic/Adapters/ConversationAdapter.java
@@ -0,0 +1,8 @@
+package tech.steampunk.kinetic.Adapters;
+
+/**
+ * Created by Vamshi on 9/9/2017.
+ */
+
+public class ConversationAdapter {
+}
--- kinetic/app/src/main/java/tech/steampunk/kinetic/UI/ChatActivity.java
+++ kinetic/app/src/main/java/tech/steampunk/kinetic/UI/ChatActivity.java
@@ -1,23 +1,37 @@
 package tech.steampunk.kinetic.UI;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import tech.steampunk.kinetic.R;
 
 public class ChatActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    @BindView(R.id.message_content)EditText message;
+    @BindView(R.id.camera_intent)ImageView camera_action;
+    @BindView(R.id.send_message)ImageView send_button;
+    @BindView(R.id.conversation_list)RecyclerView conversation_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        ButterKnife.bind(this);
         toolbar = (Toolbar)findViewById(R.id.chat_activity_toolbar);
         Intent in = getIntent();
         String name = in.getStringExtra("Name");
@@ -25,12 +39,50 @@ public class ChatActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle(name);
+        send_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(message.getText().toString().isEmpty()){
+                    Toast.makeText(ChatActivity.this, "Type a Message to send", Toast.LENGTH_SHORT).show();
+                }else {
 
+                }
+            }
+        });
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        message.requestFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
+        super.onDestroy();
     }
 }
