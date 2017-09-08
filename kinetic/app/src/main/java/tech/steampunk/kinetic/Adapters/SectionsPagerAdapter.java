0129b1147622db952ca6bbd5de8108e995aa2025
Vamshi Krishna <chsvk21@gmail.com>
2017-09-08 10:14:21 +0530

    Setup ViewPager, Working 3 Tabs, 1) Groups 2) Chats 3) Calls

---
+++ kinetic/app/src/main/java/tech/steampunk/kinetic/Adapters/SectionsPagerAdapter.java
@@ -0,0 +1,50 @@
+package tech.steampunk.kinetic.Adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import tech.steampunk.kinetic.UI.CallsFragment;
+import tech.steampunk.kinetic.UI.ChatsFragment;
+import tech.steampunk.kinetic.UI.GroupsFragment;
+
+/**
+ * Created by Vamshi on 9/8/2017.
+ */
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    private String[] titles = {"Groups", "Chats", "Calls"};
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                GroupsFragment groupsFragment = new GroupsFragment();
+                return groupsFragment;
+            case 1:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+            case 2:
+                CallsFragment callsFragment = new CallsFragment();
+                return callsFragment;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
--- kinetic/app/src/main/java/tech/steampunk/kinetic/UI/MainActivity.java
+++ kinetic/app/src/main/java/tech/steampunk/kinetic/UI/MainActivity.java
@@ -4,28 +4,45 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import tech.steampunk.kinetic.Adapters.SectionsPagerAdapter;
 import tech.steampunk.kinetic.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     @BindView(R.id.fab)Button contacts;
     private static final int READ_CONTACTS = 1;
+    private android.support.v7.widget.Toolbar toolbar;
+    private ViewPager viewPager;
+    private SectionsPagerAdapter sectionsPagerAdapter;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Kinetic");
+        tabLayout = (TabLayout)findViewById(R.id.main_tabs);
+        viewPager = (ViewPager)findViewById(R.id.ViewPager);
+        sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(sectionsPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
         ActivityCompat.requestPermissions(MainActivity.this,
                 new String[]{Manifest.permission.READ_CONTACTS},
                 READ_CONTACTS);
