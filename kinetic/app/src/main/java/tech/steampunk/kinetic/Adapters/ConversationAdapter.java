5f35f83808f86e95dbf9aa7e2bd9db993563b901
Vamshi Krishna <chsvk21@gmail.com>
2017-09-09 13:00:18 +0530

    Added messages to RecyclerView in Chat_Activity
    Need To Store Them offline and Online

--- kinetic/app/src/main/java/tech/steampunk/kinetic/Adapters/ConversationAdapter.java
+++ kinetic/app/src/main/java/tech/steampunk/kinetic/Adapters/ConversationAdapter.java
@@ -1,8 +1,55 @@
 package tech.steampunk.kinetic.Adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import tech.steampunk.kinetic.R;
+import tech.steampunk.kinetic.data.Message;
+
 /**
  * Created by Vamshi on 9/9/2017.
  */
 
-public class ConversationAdapter {
+public class ConversationAdapter extends RecyclerView.Adapter<ConversationAdapter.MyViewHolder> {
+
+    private List<Message> conversation;
+
+    public ConversationAdapter(List<Message> conversation) {
+        this.conversation = conversation;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView actual_message, message_time_stamp;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            actual_message = (TextView)itemView.findViewById(R.id.actual_message);
+            message_time_stamp = (TextView)itemView.findViewById(R.id.message_time_stamp);
+        }
+    }
+
+    @Override
+    public ConversationAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.single_message, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ConversationAdapter.MyViewHolder holder, int position) {
+        Message m = conversation.get(position);
+        holder.actual_message.setText(m.getMessage());
+        holder.message_time_stamp.setText(m.getTime());
+    }
+
+    @Override
+    public int getItemCount() {
+        return conversation.size();
+    }
 }
--- kinetic/app/src/main/java/tech/steampunk/kinetic/UI/ChatActivity.java
+++ kinetic/app/src/main/java/tech/steampunk/kinetic/UI/ChatActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -15,9 +17,16 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import tech.steampunk.kinetic.Adapters.ConversationAdapter;
 import tech.steampunk.kinetic.R;
+import tech.steampunk.kinetic.data.Message;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -26,12 +35,15 @@ public class ChatActivity extends AppCompatActivity {
     @BindView(R.id.camera_intent)ImageView camera_action;
     @BindView(R.id.send_message)ImageView send_button;
     @BindView(R.id.conversation_list)RecyclerView conversation_list;
+    private List<Message> conversation;
+    private ConversationAdapter messageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         ButterKnife.bind(this);
+        conversation = new ArrayList<>();
         toolbar = (Toolbar)findViewById(R.id.chat_activity_toolbar);
         Intent in = getIntent();
         String name = in.getStringExtra("Name");
@@ -39,13 +51,28 @@ public class ChatActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle(name);
+        messageAdapter = new ConversationAdapter(conversation);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        conversation_list.setLayoutManager(mLayoutManager);
+        conversation_list.setItemAnimator(new DefaultItemAnimator());
+        conversation_list.setAdapter(messageAdapter);
         send_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(message.getText().toString().isEmpty()){
                     Toast.makeText(ChatActivity.this, "Type a Message to send", Toast.LENGTH_SHORT).show();
                 }else {
-
+                    Date currentTime = Calendar.getInstance().getTime();
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(currentTime);
+                    int hours = calendar.get(Calendar.HOUR_OF_DAY);
+                    int minutes = calendar.get(Calendar.MINUTE);
+                    int seconds = calendar.get(Calendar.SECOND);
+                    String newTime = hours + ":" + minutes;
+                    Message t= new Message(message.getText().toString().trim(), newTime);
+                    message.setText("");
+                    conversation.add(t);
+                    messageAdapter.notifyDataSetChanged();
                 }
             }
         });
